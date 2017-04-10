59c6072f37ec1eba7bfb06db7c4575e489794316
YuliaSavchuk <yulia.savchuk3@gmail.com>
2017-04-10 11:17:36 +0300

    Add nps dialog

---
+++ app/dialogs/bindingHandlers/ratingBindingHandler.js
@@ -0,0 +1,60 @@
+﻿var css = {
+    ratingContainer: 'rating-container',
+    ratingItem: 'rating-item',
+    checked: 'checked',
+    highlighted: 'highlighted'
+},
+keys = {
+    rating: 'rating'
+};
+
+ko.bindingHandlers.rating = {
+    init: function(element, valueAccessors) {
+        var value = ko.utils.unwrapObservable(valueAccessors().value) || 0,
+            max = ko.utils.unwrapObservable(valueAccessors().max) || 5,
+            onValueUpdated = valueAccessors().onValueUpdated,
+            $element= $(element)
+        ;
+
+        var $container = $("<div></div>");
+        $container.addClass(css.ratingContainer);
+        $container.appendTo($element);
+        $container.mouseleave(function () {
+            $container.children().each(function(index, item){
+                $(item).removeClass(css.highlighted);
+            });
+        });
+
+        for (var i = 0; i < max; i++) {
+            var $item = $("<div></div>");
+            $item.addClass(css.ratingItem);
+            $item.data(keys.rating, i + 1);
+            $item.appendTo($container);
+            $item.click(function() {
+                var rating = $(this).data(keys.rating);
+                valueAccessors().value(rating);
+                applyRatingStyles(rating, css.checked);
+                if (_.isFunction(onValueUpdated)) {
+                    onValueUpdated();
+                }
+            });
+            $item.hover(function () {
+                var rating = $(this).data(keys.rating);
+                applyRatingStyles(rating, css.highlighted);
+            });
+        }
+
+        applyRatingStyles(value, css.checked);
+
+        function applyRatingStyles(rating, className){
+            $container.children().each(function(index, item){
+                var $item = $(item);
+                if(index < rating) {
+                    $item.addClass(className);
+                }else {
+                    $item.removeClass(className);
+                }
+            });
+        }
+    }
+};
---
+++ app/dialogs/nps.js
@@ -0,0 +1,45 @@
+﻿define([],
+    function () {
+        var steps = {
+            rating: 'rating',
+            thankYou: 'thankYou'
+        };
+
+        var viewModel = {
+            activate: activate,
+            submit: submit,
+            rating: ko.observable(),
+            currentStep: ko.observable(),
+            callbacks: {}
+        };
+
+        viewModel.isRatingStepShown = ko.computed(function(){
+            return viewModel.currentStep() === steps.rating;
+        });
+
+        viewModel.isThankYouStepShown = ko.computed(function(){
+            return viewModel.currentStep() === steps.thankYou;
+        });
+
+        return viewModel;
+
+        function activate(data) {
+            if (!data.close)
+                throw 'Nps dialog activation data close() method is not specified';
+
+            viewModel.close = data.close;
+            if (data.callbacks)
+                viewModel.callbacks = data.callbacks;
+
+            viewModel.rating(1);
+            viewModel.currentStep(steps.rating);
+        }
+
+        function submit() {
+            viewModel.currentStep(steps.thankYou);
+
+            if (_.isFunction(viewModel.callbacks.reported)){
+                viewModel.callbacks.reported();
+            }
+        }
+    });
